0efa3d7a30d304c233543e055843143efd1c98fa
RahmatulFirdaus <rahmatulfirdausuniska@gmail.com>
2024-10-17 13:58:45 +0800

    update

---
+++ controller/book.js
@@ -0,0 +1,7 @@
+const getBook = (req, res) =>{
+    res.json({message: 'Get book data'})
+  }
+  
+  module.exports = {
+    getBook
+  }
--- routes/book.js
+++ routes/book.js
@@ -1,9 +1,7 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const bookController = require('../controller/book');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  // const {}
-});
+router.get('/', bookController.getBook);
 
 module.exports = router;
